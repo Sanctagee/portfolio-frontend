d613d0c8c598c19a5adbdfc9a036999c8b6a2036
SanctaGee <tonygabito@gmail.com>
2026-02-22 17:38:55 +0100

    Add the admin login page

---
+++ src/pages/admin/adminLogin.jsx
@@ -0,0 +1,10 @@
+function AdminLogin() {
+  return (
+    <div className="container">
+      <h1>Admin Login</h1>
+      <p>Login form coming soon!</p>
+    </div>
+  )
+}
+
+export default AdminLogin

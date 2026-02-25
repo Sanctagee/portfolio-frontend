a8f2cdcdf48a84b92975399eb41c6e796d25ac32
SanctaGee <tonygabito@gmail.com>
2026-02-25 15:14:51 +0100

    fix: correct import path to include the parent folder

--- src/pages/admin/adminLogin.jsx
+++ src/pages/admin/adminLogin.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { useAuth } from "../../context/authContext"
-import { authAPI } from "../../api" 
+import { authAPI } from "../../services/api" 
 import "../../styles/admin/adminLogin.css"
 
 function AdminLogin() {
